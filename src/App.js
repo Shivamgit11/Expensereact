2daa17e29cdf681d1b5daba32e23bfe6b9b8fd1a
shivam <mailtoshivam2002@gmail.com>
2022-10-03 00:11:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from './components/Expenseitem';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>lets get started</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Expenseitem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+return (
+    <div>
+    <h1>Expenst Item!</h1>
+    <div>
+        <h2>Food Rs 10</h2>
+        <h2>petrol Rs 100</h2>
+        <h2>Movies Rs 200</h2>
+    </div>
+    </div>
+);
+}
+
+export default ExpenseItem;
